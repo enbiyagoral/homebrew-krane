88c8f40f46ada84528d33c26edb78d7274c88f4e
Enbiya <100806254+enbiyagoral@users.noreply.github.com>
2025-09-29 16:41:34 +0300

    Fix sha256 for v1.0.0

--- Formula/krane.rb
+++ Formula/krane.rb
@@ -2,7 +2,7 @@ class Krane < Formula
   desc "A lightweight CLI that discovers container images in your Kubernetes cluster and mirrors them to AWS ECR"
   homepage "https://github.com/enbiyagoral/krane-cli"
   url "https://github.com/enbiyagoral/krane-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "a70c50397161869c49f097346e88a10994ddb468b89da91ffbf88d7b5cae2b07"
   license "MIT"
 
   depends_on "go" => :build
